f4ff2371241a38eab44845aa7236626b1ac0db49
peterfuchs1 <peterfuchs_1@yahoo.de>
2020-10-07 08:52:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='UPOI',
+    version='1.03',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='uhs374h',
+    author_email='',
+    description='Class for communication with IGO POI\'s',
+    long_description=
+    """
+    Baseclass for interact with UPOI points of interest:
+    reading formats: upoi, txt and csv
+    writing formats: upoi, csv and gpx
+    reading from scratch or append the file content
+    """
+)
